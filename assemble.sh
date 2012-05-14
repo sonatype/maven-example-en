f174202fcc74e668fa87c691823bf8d25caaa7b3
Tim O'Brien <tobrien@discursive.com>
2012-05-14 05:17:58 -0500

    Modified the assemble.sh script to copy the requisite css files

--- assemble.sh
+++ assemble.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf target/site/reference
+rm -rf target/site/pdf
+
 pushd .
 cd examples
 mvn clean install
@@ -11,7 +14,13 @@ mkdir -p target/site/pdf
 cp examples/target/mvnexbook-examples-1.0-project.zip target/site/mvnex-examples.zip
 
 cp -r target/book-mvnex.chunked/* target/site/reference
+mkdir -p target/site/reference/css
+cp -r site/css/book.css target/site/reference/css
+
 cp target/book-mvnex.pdf target/site/pdf/mvnex-pdf.pdf
+
 python template.py
+
 cp target/site/reference/index.html target/site/reference/public-book.html
+
 rsync -e ssh -av target/site/* deployer@www.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/mvnex-book/
