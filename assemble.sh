c3373ebd12c6aafc3e04dab89cc365b7241f5a02
Manfred Moser <manfred@simpligility.com>
2013-10-30 16:37:56 -0700

    made build scripts less verbose and more robust

--- assemble.sh
+++ assemble.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# fail if anything errors
+set -e
+# fail if a function call is missing an argument
+set -u
+
 rm -rf target/site/reference
 rm -rf target/site/pdf
+
+echo "Building example projects"
 
 pushd .
 cd examples
 mvn clean install
 popd 
 
+echo "Copying resources for site"
 mkdir -p target/site/reference
 mkdir -p target/site/pdf
 
@@ -18,12 +26,13 @@ mkdir -p target/site/reference/css
 cp -r site/css target/site/reference
 cp -r site/js target/site/reference
 cp -r site/assets target/site/reference
-cp -r site/images target/site/reference
 
 cp target/book-mvnex.pdf target/site/pdf/mvnex-pdf.pdf
 
+echo "Applying website template"
 python template.py
 
 cp target/site/reference/index.html target/site/reference/public-book.html
 
+echo "Copying to production server"
 rsync -e ssh --delete -av target/site/ deployer@marketing01.int.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/mvnex-book/
--- build.sh
+++ build.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# fail if anything errors
+set -e
+# fail if a function call is missing an argument
+set -u
+
 mkdir -p target/images
 mkdir -p target/figs
 
 # Build the Single HTML Page Version
-asciidoc -o target/book-mvnex.html book-mvnex.asciidoc 
+# asciidoc -o target/book-mvnex.html book-mvnex.asciidoc 
 
 # Build the PDF
+echo "Building PDF"
 rm -rf target/images
 rm -rf target/figs
 cp -r figs target
 cp -r images target
-a2x -v -k -fpdf -dbook --dblatex-opts=" -P latex.output.revhistory=0  -s ./latex/custom-docbook.sty" -D target book-mvnex.asciidoc
+a2x -k -fpdf -dbook --dblatex-opts=" -P latex.output.revhistory=0  -s ./latex/custom-docbook.sty" -D target book-mvnex.asciidoc
+echo "done"
 
 # Build the Chunked HTML
-a2x -v -k -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1" -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target book-mvnex.asciidoc
+echo "Building Mutli Page HTML"
+a2x -k -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1" -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target book-mvnex.asciidoc
+echo "done"
