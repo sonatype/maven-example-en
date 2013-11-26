63bf2829bd9b769226bae56ae82f691c93beaa06
Manfred Moser <manfred@simpligility.com>
2013-11-26 15:24:45 -0800

    dynamically removing duplicate titles from body

--- template.py
+++ template.py
@@ -16,6 +16,15 @@ for infile in glob.glob( os.path.join(path, '*.html') ):
     title = "Table of Contents"
 
   body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
-  
+
+  h1Title = "<h1 class=\"title\">"
+  h1Close = "</h1>"
+  if h1Title in body:
+    titleInBody = body[body.index(h1Title) : body.rindex(h1Close) + len(h1Close)]
+    print ("Found title in body: %s " % titleInBody)
+    body = body.replace(bookTitle, "", 1)
+    body = body.replace(title, "", 1)
+    print ("Found bookTitle and wiped")
+    
   open(infile, "w").write( t.merge(locals()) );
 
