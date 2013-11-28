a265e8350267eb78e2ad3465d5e411569072fa4f
Manfred Moser <manfred@simpligility.com>
2013-11-27 16:07:06 -0800

    fixed layout for ToC page and all headers, automatically inserting bookId

--- template.py
+++ template.py
@@ -4,8 +4,9 @@ import os
 
 t = airspeed.Template(open("site/book-template.html", "r").read())
 bookTitle = "Maven by Example"
-bookId = "mvnrefbook"
- 
+bookId = "ss-book-mvnex"
+replaceTitleWithToC = None
+
 path = 'target/site/reference'
 for infile in glob.glob( os.path.join(path, '*.html') ):
   print "Reading File: " + infile
@@ -14,17 +15,17 @@ for infile in glob.glob( os.path.join(path, '*.html') ):
 
   if title == "Maven by Example":
     title = "Table of Contents"
+    replaceTitleWithToC = True 
 
   body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
 
-  h1Title = "<h1 class=\"title\">"
-  h1Close = "</h1>"
-  if h1Title in body:
-    titleInBody = body[body.index(h1Title) : body.rindex(h1Close) + len(h1Close)]
-    print ("Found title in body: %s " % titleInBody)
-    body = body.replace(bookTitle, "", 1)
-    body = body.replace(title, "", 1)
-    print ("Found bookTitle and wiped")
-    
+  if replaceTitleWithToC:
+    h1Title = "<h1 class=\"title\">"
+    h1Close = "</h1>"
+    if h1Title in body:
+      titleInBody = body[body.index(h1Title) : body.rindex(h1Close) + len(h1Close)]
+      print ("Found title in body: %s " % titleInBody)
+      body = body.replace(bookTitle, "Table of Contents", 1)
+      
   open(infile, "w").write( t.merge(locals()) );
 
