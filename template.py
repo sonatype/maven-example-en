5d9f898fa10380b5f6b089fc905df02fd6e4c4d6
Manfred Moser <manfred@simpligility.com>
2014-05-21 11:17:38 -0700

    avoid templating search page

--- template.py
+++ template.py
@@ -7,16 +7,19 @@ bookTitle = "Maven by Example"
 bookId = "ss-book-mvnex"
 path = 'target/site/reference'
 for infile in glob.glob( os.path.join(path, '*.html') ):
-  print "Reading File: " + infile
-  body = open(infile, "r").read()
-  title = body[ body.index( "<title>" ) + 7 : body.rindex("</title>") ]
-  body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
-
-  if "index.html" in infile:
-    print ("Replacing bookTitle  - replacing with ToC" )
-    title = "Table of Contents"
-    body = body.replace(bookTitle, title)
-    
-  open(infile, "w").write( t.merge(locals()) );
+  if infile.endswith( 'search.html'):
+    print( "  Ignoring search.html" )
+  else:
   
+    print "Reading File: " + infile
+    body = open(infile, "r").read()
+    title = body[ body.index( "<title>" ) + 7 : body.rindex("</title>") ]
+    body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
+  
+    if "index.html" in infile:
+      print ("Replacing bookTitle  - replacing with ToC" )
+      title = "Table of Contents"
+      body = body.replace(bookTitle, title)
+      
+    open(infile, "w").write( t.merge(locals()) );
 
