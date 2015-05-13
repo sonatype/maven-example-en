b1f2e36ee28bf91438c93fd93f17841d09435d62
Manfred Moser <manfred@simpligility.com>
2015-05-13 16:11:22 -0700

    added separate deployment scripts

---
+++ deploy-to-production.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# fail if anything errors
+set -e
+# fail if a function call is missing an argument
+set -u
+
+function rsyncToDest {
+    source=$1
+    target=/var/www/domains/sonatype.com/www/shared/books/$2
+    options=$3
+    connection=deployer@marketing01.int.sonatype.com
+    echo "Uploading $1 to $2 on $connection"
+    ssh $connection mkdir -pv $target
+    rsync -e ssh $options -av target/$source  $connection:$target
+}
+
+rsyncToDest site/ mvnex-book/ --delete
+
+
+
